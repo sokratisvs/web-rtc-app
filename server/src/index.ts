3723fc692216407ba5f0b9939a900df31e1a1b0a
Σωκράτης Β <sokratisvs@gmail.com>
2022-06-12 19:17:39 +0300

    server & socket setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,32 @@
-console.log('iniital setup');
+import express from 'express';
+import { createServer } from "http";
+import { Server, Socket } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8080;
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+app.get('/', (_req, res) => {
+    res.send({ message: 'Hello World!!'});
+});
+
+io.on("connection", (socket: Socket) => {
+    console.log('user is connected!');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+httpServer.listen(PORT, () => {
+    console.log(`Application listening on port ${PORT}!`);
+});
